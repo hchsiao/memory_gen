eb893d6639a85c90640299b46bce758d5f3b3256
hchsiao <hchsiao@vlsilab.ee.ncku.edu.tw>
2018-06-05 22:06:58 +0800

    support synthesis

--- cmake/FindTSMC90Memory.cmake
+++ cmake/FindTSMC90Memory.cmake
@@ -1,2 +1,4 @@
 set(TSMC90Memory_RF_1P_BIN rf_sp_adv)
 set(TSMC90Memory_SRAM_1P_BIN sram_sp_adv)
+set(LC_SHELL_BIN lc_shell)
+set(SYN_LIB_POSTFIX "_tt_1.0_25.0_syn.lib")
--- src/lib2db.tcl
+++
@@ -1,3 +0,0 @@
-read_lib  SRAM_LB_tt_1.0_25.0_syn.lib
-write_lib SRAM_LB -output SRAM_LB_tt_1.0_25.0_syn.db
-exit
